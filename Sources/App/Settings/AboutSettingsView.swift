7f972f948cf0e008aa6e608f306b348b09c81323
trungdv-fabbi <trungdv@fabbi.io>
2026-07-14 18:18:00 +0700

    #fix bug and add layout

--- Sources/App/Settings/AboutSettingsView.swift
+++ Sources/App/Settings/AboutSettingsView.swift
@@ -39,7 +39,7 @@ struct AboutSettingsView: View {
                     Text("BerryShot")
                         .font(.system(size: 28, weight: .bold))
                     
-                    Text("Version 1.1.6")
+                    Text("Version 1.1.8")
                         .font(.subheadline)
                         .foregroundColor(.secondary)
                     
